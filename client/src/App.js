a04f33326af36d879b3d663dab35de502eadd442
Angel Contreras <angelcbenoit@gmail.com>
2018-09-13 08:45:43 -0700

    begin structuring landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Login from './components/login';
 import Profile from './components/profile';
+import LandingPage from './components/LandingPage';
 import {BrowserRouter,Route} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from './Actions';
@@ -16,6 +17,7 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <div>
+            <Route exact path='/' component={LandingPage} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/profile' component={Profile} />
           </div>
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="landing__header">
+                    <div className="landing__header--text">
+                        <h1 className="landing__header--title">Social Media</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
