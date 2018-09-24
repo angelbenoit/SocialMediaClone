9a3b84532f54baf556cb4bc22c262ac3d66d94cb
Angel Contreras <angelcbenoit@gmail.com>
2018-09-24 09:29:49 -0700

    update post page header

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import postImage from '../images/post.png';
 
 class Posts extends Component {
     render() {
@@ -6,12 +7,27 @@ class Posts extends Component {
             <div>
                 <div className="posts__header">
                     <div className="posts__header--text">
-                        POSTS
+                        <h2 className="heading-secondary">Posts</h2>
+                    </div>
+
+                    <div className="posts__header--instructions">
+                        <div className="instructions">
+                            <h4>How to create a new post</h4>
+                            <p>
+                               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                               Unde, consectetur molestias. Sunt nobis, excepturi quam
+                               dolores voluptates rem vitae, corporis
+                               iste ratione nostrum, assumenda earum sed id cum in quaerat
+                            </p>
+                        </div>
+                        <div className="instructions">
+                            <img src={postImage} alt="socializing" className="composition__photo2" />
+                        </div>
                     </div>
                 </div>
 
-                 <div className="posts__list">
-                 </div>
+                <div className="posts__list">
+                </div>
             </div>
         );
     }
