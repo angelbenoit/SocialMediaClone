f6f315ab35c4826a0ef2e10a2a32ce6bfb357b36
Angel Contreras <angelcbenoit@gmail.com>
2018-09-26 18:43:53 -0700

    set up post item display

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class PostItem extends Component {
     render() {
         return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>By {this.props.name}</h2>
+            <div className="postItem">
+                <div className="postItem__title u-center-text"><h1>{this.props.title}</h1></div>
+                <div className="postItem__author">
+                    <p>Rating: {(Math.random() * 1000).toFixed(0)}</p>
+                    <h3>Posted by: {this.props.name}</h3>
+                    <h5>{new Date().toDateString()}</h5>
+                </div>
+                <div className="postItem__comments u-center-text">
+                    <p>{(Math.random() * 1000).toFixed(0)} comments</p>
+                </div>
             </div>
         );
     }
