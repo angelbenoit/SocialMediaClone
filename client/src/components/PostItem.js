dbcf7b617faef44a77ae618fbb60a3f675f0743a
Angel Contreras <angelcbenoit@gmail.com>
2018-10-26 10:46:30 -0700

    style comment form

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -13,7 +13,6 @@ class PostItem extends Component {
             <div className="postItem" onClick={() => this.redirectToPost(this.props.id)}>
                 <div className="postItem__title u-center-text"><h1>{this.props.title}</h1></div>
                 <div className="postItem__author">
-                    <p>Rating: {(Math.random() * 1000).toFixed(0)}</p>
                     <h3>Posted by: <i>{this.props.name}</i></h3>
                     <h5>{this.props.date}</h5>
                 </div>
